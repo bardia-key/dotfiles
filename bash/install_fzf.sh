0896c87071781022220c1aca9c460b0e75e085ba
Bardia Keyoumarsi <bardia@easypost.com>
2025-08-21 11:37:27 -0700

    major clean up using opencode

--- bash/install_fzf.sh
+++ bash/install_fzf.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf
-${HOME}/.fzf/install --all
+set -e
+
+# Only install if FZF doesn't already exist
+if [ ! -d "${HOME}/.fzf" ]; then
+    echo "Installing FZF..."
+    git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf
+    ${HOME}/.fzf/install --all
+else
+    echo "FZF already installed, skipping..."
+fi
--- vim/setup_plugins.sh
+++ vim/setup_plugins.sh
@@ -1,25 +1,46 @@
 #!/bin/bash
 
+set -e
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <plugins_file>"
+    exit 1
+fi
+
+PLUGINS_FILE="$1"
+if [ ! -f "$PLUGINS_FILE" ]; then
+    echo "Error: Plugin file '$PLUGINS_FILE' not found"
+    exit 1
+fi
+
+echo "Setting up vim plugins..."
+mkdir -p ${HOME}/.vim/pack/plugins/start/
 pushd ${HOME}/.vim/pack/plugins/start/
 
 # install plugins
 while read plugin; do
-    git clone $plugin 2>/dev/null
-done < $1
-
-# They seem to have fixed the master but keeping this handy
-# pin vim-go on 1.10 since recent versions break
-# go env pickup
-# pushd ./vim-go
-# git checkout v1.10 #2>/dev/null
-# popd
+    # Skip empty lines and comments
+    [[ -z "$plugin" || "$plugin" =~ ^#.* ]] && continue
+    
+    plugin_name=$(basename "$plugin" .git)
+    if [ ! -d "$plugin_name" ]; then
+        echo "Installing $plugin_name..."
+        git clone $plugin 2>/dev/null || echo "Failed to clone $plugin"
+    else
+        echo "$plugin_name already exists, skipping..."
+    fi
+done < "$PLUGINS_FILE"
 
 # update any existing ones
+echo "Updating existing plugins..."
 for plugin in */; do
-    # if [ "$plugin" != "vim-go/" ] uncomment if ever need to pin vim-go again
-    pushd $plugin
-    git pull
-    popd
+    if [ -d "$plugin/.git" ]; then
+        echo "Updating $plugin..."
+        pushd $plugin
+        git pull || echo "Failed to update $plugin"
+        popd
+    fi
 done
 
 popd
+echo "Plugin setup complete!"
