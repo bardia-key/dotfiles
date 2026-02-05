96a181ad5d27798e8be4f37d9ae729cc3cc5209f
Bardia Keyoumarsi <bardia@easypost.com>
2026-02-05 17:18:25 +0000

    configure toggleterm with my preferences

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,1 +1,29 @@
-return {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  opts = {
+    size = function(term)
+      if term.direction == "horizontal" then
+        return 25
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.25
+      end
+    end,
+    hide_numbers = true,
+    shade_terminals = true,
+    start_in_insert = true,
+    insert_mappings = true,
+    terminal_mappings = true,
+    persist_size = true,
+    direction = 'horizontal',
+    close_on_exit = true,
+    shell = vim.o.shell,
+  },
+  keys = {
+    -- Horizontal terminal with 40 line height
+    { "<leader>th", "<cmd>ToggleTerm size=25 direction=horizontal<cr>", desc = "Terminal (horizontal)" },
+    -- Additional useful terminal shortcuts
+    { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Terminal (float)" },
+    { "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Terminal (vertical)" },
+  },
+}
