34d1cf5bb7e1ac061c191803332606afa772572b
Bardia Keyoumarsi <bardia@easypost.com>
2026-05-28 21:23:09 -0700

    fix nvim-treesiter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,41 +1,11 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-          ensure_installed = {
-            "bash",
-            "c",
-            "diff",
-            "go",
-            "html",
-            "javascript",
-            "jsdoc",
-            "json",
-            "jsonc",
-            "lua",
-            "luadoc",
-            "luap",
-            "markdown",
-            "markdown_inline",
-            "printf",
-            "python",
-            "query",
-            "regex",
-            "ruby",
-            "toml",
-            "tsx",
-            "typescript",
-            "vim",
-            "vimdoc",
-            "xml",
-            "yaml",
-          },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
+    -- nvim-treesitter v1.x: highlight and indent are enabled by default.
+    -- Install parsers with :TSInstall <lang> or :TSInstall all
+    config = function()
+        -- No setup() needed in v1.x; highlight/indent are on by default.
+        -- Register any language aliases if needed:
+        -- vim.treesitter.language.register("ruby", "eruby")
+    end,
 }
